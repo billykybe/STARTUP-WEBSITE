58821a0b3904fb5cafaadb6d2cba22453242da66
BILLY K <billykybe@gmail.com>
2023-01-03 10:44:14 +0300

    Assets added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <NavBar />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./NavBar.css";
+// Images Import
+import locationIC from "../../assets/icon/map25.png";
+import phoneIC from "../../assets/icon/phone.svg";
+// --> Images Import
+function NavBar() {
+  return (
+    <div className="navbar-container">
+      <div>
+        <p>
+          <img src="" alt="" /> CDC NAIROBI Kenya
+        </p>
+        <p>
+          <img src="" alt="" />
+          +254 4567890
+        </p>
+      </div>
+      <div></div>
+    </div>
+  );
+}
+
+export default NavBar;
